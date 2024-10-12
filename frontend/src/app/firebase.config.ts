61784b05fe5b9cb2dc1daf31ee803c9950362666
KhoonSun47 <justforwks@gmail.com>
2024-10-12 15:26:35 +0800

    Separate the configs and add .gitignore

---
+++ frontend/src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCw5q1BUiLo8S6BzO9hU5kAKyw05obooPs",
+  authDomain: "pierpressure-5a9cf.firebaseapp.com",
+  projectId: "pierpressure-5a9cf",
+  storageBucket: "pierpressure-5a9cf.appspot.com",
+  messagingSenderId: "333517113388",
+  appId: "1:333517113388:web:e17189bc92490b1618a989"
+};
