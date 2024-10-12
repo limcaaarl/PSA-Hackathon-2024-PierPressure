175ce0c38ef2eada69e812ed7fce441ac1402566
limcaaarl <carljovi22@gmail.com>
2024-10-12 20:38:54 +0800

    Bind firestore data with table

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideHttpClient } from "@angular/common/http";
 import { provideFirebaseApp, initializeApp } from "@angular/fire/app";
 import { getAuth, provideAuth } from "@angular/fire/auth";
 import { firebaseConfig } from './firebase.config';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth()),
+    provideAuth(() => getAuth()), provideFirebaseApp(() => initializeApp({"projectId":"pierpressure-5a9cf","appId":"1:333517113388:web:e17189bc92490b1618a989","storageBucket":"pierpressure-5a9cf.appspot.com","apiKey":"AIzaSyCw5q1BUiLo8S6BzO9hU5kAKyw05obooPs","authDomain":"pierpressure-5a9cf.firebaseapp.com","messagingSenderId":"333517113388"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 };
