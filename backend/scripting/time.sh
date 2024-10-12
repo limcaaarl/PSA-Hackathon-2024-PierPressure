b02f5020689f7d34e04640d7b6bdd3e5ff62ce4f
KhoonSun47 <justforwks@gmail.com>
2024-10-12 23:48:19 +0800

    Added Docker for Backend

--- backend/scripting/time.sh
+++ backend/scripting/time.sh
@@ -6,8 +6,8 @@ pip install -r requirement.txt
 # Run the test.py script and save output in JSON format
 # python3 test.py >> output.json 2>> logfile.log
 
-# Add cron job
-echo "*/15 * * * * /usr/bin/python3 /app/test.py >> /app/output.json 2>> /app/logfile.log" | crontab -
+# Add cron job - the test.py need to change to scraper_lim.py
+echo "*/1 * * * * /usr/bin/python3 /app/test.py >> /app/output.json 2>> /app/logfile.log" | crontab -
 
-Start cron service in foreground
+#Start cron service in foreground
 cron -f
